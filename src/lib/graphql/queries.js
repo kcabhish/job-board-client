8a3761e2a8c59da4f489b418fed89b307237d43e
kcabhish <kcabhish@gmail.com>
2024-03-15 03:55:15 -0400

    feat: initial commit for the client

---
+++ src/lib/graphql/queries.js
@@ -0,0 +1,40 @@
+import { GraphQLClient, gql } from "graphql-request";
+
+const graphQLUrl = 'http://localhost:9000/graphql';
+const client = new GraphQLClient(graphQLUrl);
+
+export async function getJob(id) {
+  const query = gql`
+    query jobById($id: ID!) {
+      job(id: $id) {
+        id,
+        date,
+        title,
+        company {
+          id,
+          name
+        }
+        description
+      }
+    }
+  `;
+  const { job } = await client.request(query, { id });
+  return job;
+}
+export async function getJobs() {
+    const query = gql`
+        query {
+        jobs {
+          company {
+            id
+            name
+          }
+          date
+          title
+          id
+        }
+      }`;
+
+    const { jobs } = await client.request(query);
+    return jobs;
+}
---
+++ src/pages/HomePage.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import JobList from '../components/JobList';
+import { getJobs } from '../lib/graphql/queries';
+
+function HomePage() {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    getJobs().then((data) => setJobs(data))
+  },[]);
+
+  console.log('[HomePage] jobs:', jobs);
+  return (
+    <div>
+      <h1 className="title">
+        Job Board
+      </h1>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default HomePage;
